e9a836ffaa0cb09f473d3c1902e7bcb9c75abba2
STong01 <1461638919@qq.com>
2019-09-20 21:31:18 +0800

    。。。

---
+++ C+.0920/C+.0920/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+//实现一个类，计算机中创建了多少个类
+//class A{
+//public:
+//	A(){ ++_scount; }
+//	A(const A&t){ ++_scount; }
+//	static int GetACount(){ return _scount; }
+//private:
+//	static int _scount;
+//};
+//
+//class Test{
+//
+//	static int _count;
+//};
+//
+//int Test::_count = 0;
+//
+//void TestA(){
+//	cout << A::GetACount() << endl;
+//	A a1, a2;
+//	A a3(a1);
+//	cout << A::GetACount() << endl;
+//}
+
+
+int main(){
+
+	system("pause");
+	return 0;
+}
